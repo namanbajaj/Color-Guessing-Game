d258ee7e749501279594aa4927e0707346f6c8e4
namanbajaj <namanbajaj05@gmail.com>
2022-06-10 13:29:07 -0400

    high score working

--- app/src/main/java/com/example/colorguessinggame/GameScreen.kt
+++ app/src/main/java/com/example/colorguessinggame/GameScreen.kt
@@ -1,5 +1,6 @@
 package com.example.colorguessinggame
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
@@ -10,7 +11,9 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import org.w3c.dom.Text
+import java.io.File
 import kotlin.collections.ArrayList
+import kotlin.math.abs
 
 
 class GameScreen : AppCompatActivity() {
@@ -19,7 +22,45 @@ class GameScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_screen)
 
-        fun List<Int>.closestValue(value: Int) = minByOrNull { kotlin.math.abs(value - it) }
+        // load high score
+        val highScoreField = findViewById<TextView>(R.id.highScoreText)
+
+//        val path = this.getFilesDir()
+        val highScoreFile = File(this.filesDir, "UserScore.txt")
+
+        var highScore = -1
+
+        // first time playing game
+        if(!highScoreFile.exists()) {
+            highScoreFile.createNewFile()
+            highScoreField.setText("High Score: 0")
+        }
+        else{
+            // read in high score
+//            this.openFileInput("UserScore.txt").bufferedReader().useLines { lines ->
+//                lines.fold("") {
+//                        some, text -> "$some\n$text"
+//                }
+//                Log.i("lines", lines.toString())
+
+            this.openFileInput("UserScore.txt").use { stream ->
+                val text = stream.bufferedReader().use {
+                    it.readText()
+                }
+                Log.i("from high score file", "LOADED: $text")
+
+                highScore = text.toInt()
+                highScoreField.setText("High Score: " + highScore.toString())
+            }
+//
+//            }
+//            val test = File("UserScore.txt").inputStream().reader().use { it.readText() }
+//            Log.i("from high score file", test)
+        }
+
+//        Log.i("file dir.", highScore.toString())
+
+        fun List<Int>.closestValue(value: Int) = minByOrNull { abs(value - it) }
 
         // square that changes color
         val square = findViewById<Button>(R.id.colorSquare)
@@ -46,17 +87,17 @@ class GameScreen : AppCompatActivity() {
         for(l in line){
             val s = l.split("\t")
             if(s.size >= 2) {
-                Log.i("split 1", s[0])
-                Log.i("split 2", s[1])
+//                Log.i("split 1", s[0])
+//                Log.i("split 2", s[1])
 
                 colorNames.add(s[0])
                 val rgbSplit = s[1].split(" ")
                 val redS = rgbSplit[0].trim()
                 val greenS = rgbSplit[1].trim()
                 val blueS = rgbSplit[2].trim()
-                Log.i("r", redS)
-                Log.i("g", greenS)
-                Log.i("b", blueS)
+//                Log.i("r", redS)
+//                Log.i("g", greenS)
+//                Log.i("b", blueS)
 
                 val red : Int = if(redS  == "0")
                     0
@@ -74,11 +115,11 @@ class GameScreen : AppCompatActivity() {
                     blueS.toInt()
 
                 val cToAdd = Color.rgb(red, green, blue)
-                Log.i("Converted to color", cToAdd.toString())
+//                Log.i("Converted to color", cToAdd.toString())
                 colorValues.add(cToAdd)
             }
         }
-        Log.i("Check for size", colorValues.size.toString() + " " + colorNames.size.toString())
+//        Log.i("Check for size", colorValues.size.toString() + " " + colorNames.size.toString())
 
 //        var userClicked: Boolean
 
@@ -88,7 +129,7 @@ class GameScreen : AppCompatActivity() {
         if (closestVal != null) {
             square.setBackgroundColor(closestVal)
         }
-        Log.i("Check for nearest", "old val: $newCol closest val: $closestVal")
+//        Log.i("Check for nearest", "old val: $newCol closest val: $closestVal")
 
         // find index of nearest value
         var indexOfClosest = 0
@@ -96,8 +137,8 @@ class GameScreen : AppCompatActivity() {
             if(colorValues[i] == closestVal)
                 indexOfClosest = i
 
-        Log.i("Color of closest", colorNames[indexOfClosest])
-        Log.i("Index of right answer", indexOfClosest.toString())
+//        Log.i("Color of closest", colorNames[indexOfClosest])
+//        Log.i("Index of right answer", indexOfClosest.toString())
 
         // correct answer choice
         var rightAnswer = colorNames[indexOfClosest]
@@ -119,7 +160,7 @@ class GameScreen : AppCompatActivity() {
                 colors.add(indexOfClosest)
         }
 
-        Log.i("list of options", colors.toString())
+//        Log.i("list of options", colors.toString())
 
         // which option box will contain the right answer
         var rightChoice = (1..4).random()
@@ -168,6 +209,7 @@ class GameScreen : AppCompatActivity() {
             if(rightChoice == 1)
                 score = score + 1
             else {
+                gameOver(score, highScore)
                 val intent = Intent(this, FinalScore::class.java)
                 intent.putExtra("Score", score.toString())
                 startActivity(intent)
@@ -250,6 +292,7 @@ class GameScreen : AppCompatActivity() {
             if(rightChoice == 2)
                 score = score + 1
             else {
+                gameOver(score, highScore)
                 val intent = Intent(this, FinalScore::class.java)
                 intent.putExtra("Score", score.toString())
                 startActivity(intent)
@@ -331,6 +374,7 @@ class GameScreen : AppCompatActivity() {
             if(rightChoice == 3)
                 score = score + 1
             else {
+                gameOver(score, highScore)
                 val intent = Intent(this, FinalScore::class.java)
                 intent.putExtra("Score", score.toString())
                 startActivity(intent)
@@ -412,6 +456,7 @@ class GameScreen : AppCompatActivity() {
             if(rightChoice == 4)
                 score = score + 1
             else {
+                gameOver(score, highScore)
                 val intent = Intent(this, FinalScore::class.java)
                 intent.putExtra("Score", score.toString())
                 startActivity(intent)
@@ -496,5 +541,13 @@ class GameScreen : AppCompatActivity() {
         var color = Color.rgb(red, green, blue)
 //        Log.i("COLORS", color.toString())
         return color
+    }
+
+    fun gameOver(curScore : Int, highScore : Int) {
+        if(curScore > highScore){
+            this.openFileOutput("UserScore.txt", Context.MODE_PRIVATE).use {
+                it.write(curScore.toString().toByteArray())
+            }
+        }
     }
 }
